0a576a0c126f92cd210f64664facfe1bfae1985b
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-07-28 16:09:58 +0500

    Verify tests

--- my_list.rb
+++ my_list.rb
@@ -1,13 +1,20 @@
 require_relative 'enumerable'
 
 class MyList
-    include MyEnumerable
-  
-    def initialize(list)
-      @list = list
-    end
-  
-    def each(&block)
-        @list.each(&block)
-      end
+  include MyEnumerable
+
+  def initialize(list)
+    @list = list
   end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+mylist = MyList.new([1, 2, 3, 4, 5])
+puts(mylist.all? { |e| e < 5 })
+puts(mylist.all? { |e| e > 5 })
+puts(mylist.any? { |e| e == 2 })
+puts(mylist.any? { |e| e == 5 })
+p(mylist.filter(&:even?))
